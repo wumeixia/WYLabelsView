b21912c5d4d6795def18cf1efb1f8f3192a648e3
王玉 <wangyu@wangyudemacbook-pro.local>
2016-06-27 21:53:02 +0800

    Initial Commit

---
+++ WYLabelsView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYLabelsView
+//
+//  Created by 王玉 on 16/6/27.
+//  Copyright © 2016年 王玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYLabelsView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WYLabelsView
+//
+//  Created by 王玉 on 16/6/27.
+//  Copyright © 2016年 王玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
